2242f3143074bf8589eab82872367c06bb206b10
Andrew Harris <126024439+TAMUsquirrel@users.noreply.github.com>
2023-03-07 15:43:33 -0600

    Update ProperSentenceCase_1.1.1-works.py

--- ProperSentenceCase_1.1.1-works.py
+++ ProperSentenceCase_1.1.1-works.py
@@ -9,7 +9,7 @@ def pwise(iterable):
     next(b, None)
     return zip(a, b)
 
-with st.sidebar:
+with st.sidebar():
     about_head = st.subheader("About")
     about_info = st.text("This app uses Natural Language Processing provided by Stanza (CoreNLP) to capitalize named entities, in addition to a host of other common English capitalization rules.")
         
