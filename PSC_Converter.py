06fd14e70837c5e01962fe4b5eaaf2fd1f794e6b
Andrew Harris <126024439+TAMUsquirrel@users.noreply.github.com>
2023-03-24 16:01:04 -0500

    Update PSC_Converter.py

--- PSC_Converter.py
+++ PSC_Converter.py
@@ -48,9 +48,13 @@ elif len(NLP_Dict_1[0]) == 1:
     final_output = Draft1_acronyms_capped
 else:
     Sents_Capped = sentence_1st_char_capitalizer(NLP_Dict_1, sentence_2nd_char_capitalizer(NLP_Dict_1, double_spaces_removed))
+    i_capped = re.sub("( )(i)( |\?)", celtic_basic_capitalizer, Sents_Capped, flags=re.IGNORECASE)
+    with open('first_names_resub') as firstnames:
+        first_names = firstnames.read()
+        first_names_capped = re.sub(f"( )({first_names})( |\'|\'s|\.|\?|\!)", mid_capitalizer, i_capped, flags=re.IGNORECASE)
 
     List_of_Single_Word_Titles = ['agent', 'brother', 'cantor', 'captain', 'chairperson', 'chancellor', 'chef', 'chief', 'commissioner', 'darth', 'dame', 'dean', 'deputy', 'detective', 'director', 'doctor', 'father', 'governor', 'judge', 'king', 'queen', 'prince', 'princess', 'czar', 'lady', 'laird', 'lieutenant', 'lord', 'madame', 'master', 'miss', 'officer', 'pastor', 'president', 'principal', 'professor', 'provost', 'rabbi', 'rector', 'regent', 'reverend', 'saint', 'sensei', 'sheriff', 'sister', 'student', 'trainer', 'warden']
-    Basics_Capped = Capitalize_Word2(NLP_Dict_1, Capitalize_Word1(NLP_Dict_1, Sents_Capped))
+    Basics_Capped = Capitalize_Word2(NLP_Dict_1, Capitalize_Word1(NLP_Dict_1, first_names_capped))
     titles_capped = title_triwise_front_capper(NLP_Dict_1, title_quadwise_back_capper(NLP_Dict_1, Basics_Capped))
 
     capitalize_celtic = re.sub("( )(Mc|Mac|O\')([a-z][a-z][a-z])", celtic_basic_capitalizer, titles_capped, flags=re.IGNORECASE)
